9ac347e619acf4b26394c1cca06d1119ea85ba78
Eric Booker <ericbooker12@gmail.com>
2016-09-11 19:24:35 -0700

    Seed second well

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
 # # # Seed data from file
 require 'faker'
 
-Rig.delete_all
-Well.delete_all
-Measurement.delete_all
+# Rig.delete_all
+# Well.delete_all
+# Measurement.delete_all
 
 rows = []
-File.foreach("depth1.txt") do |line|  # read in one line/row at a time
+File.foreach("depth2.txt") do |line|  # read in one line/row at a time
 	row = line.split(" ") 			# convert each row to an array
 
 	i = 0
@@ -26,44 +26,52 @@ rows.each do |row|					#[69.00, 139.30, 35.90, 54.30, 58.42, 635.66]
 		temp_in: row[3],
 		temp_out: row[4],
 		pressure: row[5],
-		well_id: 1
+		well_id: 2
 	) 
 	
 end
 
-# Seed well data
 Well.create(
-	name: "Happy Jack 12",
+	name: "Well 2",
 	location: "N 416524.0, E 1769412.4",
-	start_date: "2/6/2009",
-	end_date: "3/24/2009",
-	total_depth: 7622
+	start_date: "2/6/2015",
+	end_date: "3/24/2015",
+	total_depth: 9999
 ) 
 
-# Seed rig data
-Rig.create(
-	name: "Rig 3",
-	company: "Kenai"
-)
+# Seed well data
+# Well.create(
+# 	name: "Happy Jack 12",
+# 	location: "N 416524.0, E 1769412.4",
+# 	start_date: "2/6/2009",
+# 	end_date: "3/24/2009",
+# 	total_depth: 7622
+# ) 
 
-10.times do 
-	Rig.create(
-		name: "Rig #{Faker::Number.number(1)}",
-		company: Faker::Company.name
-	)
+# # Seed rig data
+# Rig.create(
+# 	name: "Rig 3",
+# 	company: "Kenai"
+# )
 
-end
+# 10.times do 
+# 	Rig.create(
+# 		name: "Rig #{Faker::Number.number(1)}",
+# 		company: Faker::Company.name
+# 	)
+
+# end
 
 # Seed well data
-10.times do |create|
-	Well.create(
-		name: "#{Faker::Name.first_name}-#{Faker::Number.number(2)}",
-		location: "N 416524.0, E 1769412.4",
-		start_date: Faker::Date.between(2.years.ago, 1.year.ago),
-		end_date: Faker::Date.between(1.year.ago, Date.today),
-		total_depth: Faker::Number.number(4)
-	)
-end
+# 10.times do |create|
+# 	Well.create(
+# 		name: "#{Faker::Name.first_name}-#{Faker::Number.number(2)}",
+# 		location: "N 416524.0, E 1769412.4",
+# 		start_date: Faker::Date.between(2.years.ago, 1.year.ago),
+# 		end_date: Faker::Date.between(1.year.ago, Date.today),
+# 		total_depth: Faker::Number.number(4)
+# 	)
+# end
 
 # Seed user data
 # 10.times do
