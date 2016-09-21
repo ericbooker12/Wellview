8624e849b91fc3f3dc57cb8bf1152f1158eb3b12
Eric Booker <ericbooker12@gmail.com>
2016-09-20 18:55:10 -0700

    create controller and view for main page, add css styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+  get 'measurement/index'
+
+  resources :measurements
+
+  get 'welcome/index'
+
+  root 'main#index'
+  end
